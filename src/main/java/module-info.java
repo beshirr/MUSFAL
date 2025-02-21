85b028591cff1d3209c31b490e8bc92a701da638
beshir <youssefahmedbeshir@gmail.com>
2025-02-21 13:19:31 +0200

    module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.openjfx.workoutplanner {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires com.google.gson;
 
     opens org.openjfx.workoutplanner to javafx.fxml;
     exports org.openjfx.workoutplanner;
