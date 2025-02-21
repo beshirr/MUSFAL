1a34a535562b5acd20a2c02bdb8dd6a605f587f8
beshir <youssefahmedbeshir@gmail.com>
2025-02-21 13:20:14 +0200

    Read from json method

--- src/main/java/org/openjfx/workoutplanner/ReadExerciseJson.java
+++ src/main/java/org/openjfx/workoutplanner/ReadExerciseJson.java
@@ -1,6 +1,27 @@
 package org.openjfx.workoutplanner;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
-public class FetchExerciseApi {
-    String apiUrl = "https://exercisedb-api.vercel.app/api/v1/exercises";
+import java.io.FileReader;
+import java.io.IOException;
 
+
+public class ReadExerciseJson {
+    public static JsonArray readExerciseJson() {
+        try {
+            FileReader reader = new FileReader("exercises.json");
+
+            Gson gson = new Gson();
+            JsonObject data = gson.fromJson(reader, JsonObject.class);
+
+            JsonArray exercises = data.getAsJsonArray("exercises");
+            reader.close();
+            return exercises;
+        }
+        catch (IOException e) {
+            System.out.println("Error reading exercises.json");;
+        }
+        return null;
+    }
 }
