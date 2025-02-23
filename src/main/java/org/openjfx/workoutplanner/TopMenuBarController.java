fb1ab9d6545f7272ab5779f1fa76405b2d0feeae
beshir <youssefahmedbeshir@gmail.com>
2025-02-23 07:50:43 +0200

    added the exercises tab in the menu bar

--- src/main/java/org/openjfx/workoutplanner/TopMenuBarController.java
+++ src/main/java/org/openjfx/workoutplanner/TopMenuBarController.java
@@ -1,9 +1,8 @@
 package org.openjfx.workoutplanner;
 
 import javafx.event.ActionEvent;
-
-
 import java.io.IOException;
+
 
 public class TopMenuBarController {
 
@@ -11,8 +10,8 @@ public class TopMenuBarController {
         SceneController.switchScene("bmiCalculator.fxml", "BMI Calculator");
     }
 
-    public void handleBFI(ActionEvent actionEvent) throws IOException {
-        SceneController.switchScene("bmiCalculator.fxml",  "Body Fat Index Calculator");
+    public void handleBFP(ActionEvent actionEvent) throws IOException {
+        SceneController.switchScene("bfpCalculator.fxml",  "Body Fat Index Calculator");
     }
 
     public void handleCB(ActionEvent actionEvent) throws IOException {
@@ -25,5 +24,10 @@ public class TopMenuBarController {
 
     public void handleORMC(ActionEvent actionEvent) throws IOException {
         SceneController.switchScene("bmiCalculator.fxml", "One Rep Max Calculator");
+    }
+
+    public void handleExercises(ActionEvent actionEvent) throws IOException {
+        SceneController.switchScene("exercises.fxml", "Exercises");
+        System.out.println("Exercises pressed");
     }
 }
