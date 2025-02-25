187125dd199552c81c29c6ac7da52b1f258384ba
beshir <youssefahmedbeshir@gmail.com>
2025-02-25 20:45:55 +0200

    updated the top menu

--- src/main/java/org/openjfx/workoutplanner/TopMenuBarController.java
+++ src/main/java/org/openjfx/workoutplanner/TopMenuBarController.java
@@ -14,20 +14,15 @@ public class TopMenuBarController {
         SceneController.switchScene("bfpCalculator.fxml",  "Body Fat Index Calculator");
     }
 
-    public void handleCB(ActionEvent actionEvent) throws IOException {
-        SceneController.switchScene("bmiCalculator.fxml", "Calories Burned Calculator");
-    }
-
     public void handleDCC(ActionEvent actionEvent) throws IOException {
         SceneController.switchScene("bmrCalculator.fxml", "Daily Calories Calculator");
     }
 
     public void handleORMC(ActionEvent actionEvent) throws IOException {
-        SceneController.switchScene("bmiCalculator.fxml", "One Rep Max Calculator");
+        SceneController.switchScene("ormCalculator.fxml", "One Rep Max Calculator");
     }
 
     public void handleExercises(ActionEvent actionEvent) throws IOException {
         SceneController.switchScene("exercises.fxml", "Exercises");
-        System.out.println("Exercises pressed");
     }
 }
